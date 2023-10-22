ff377845a2dde95ec387fd5c5cf8d78386ec2ee2
Angie <angelam202@hotmail.com>
2023-10-22 18:01:19 -0500

    work on js code on local storage section

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,21 +4,29 @@ $(document).ready(function () {
     var today = dayjs().format('dddd, MMM D, YYYY h:mm A');
     $("#currentDay").html(today);
 
+    // save text to local storage
     $(".saveBtn").on("click", function () {
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
+        localStorage.setItem(time, text);
+    })
 
-        // save text to local storage
-        window.localStorage.setItem(time, text);
-    });
+    // retrives text from local storage
+    for (var i = 8; i <= 18; ++i) {
+      var saved = localStorage.getItem("hour-" + i);
+      var textArea = $("#hour-" + i + " .description");
+
+      if (saved !== null) {
+        textArea.val(saved)
+      }
+   }
 
     // function to change colors of time blocks depending on past, present, future
-    function timeTense() {
+    function colorTracker() {
          var now = dayjs().hour();
 
         $(".time-block").each(function () {
             var blockTime = parseInt($(this).attr("id").split("hour-")[1]);
-
             
             if (blockTime < now) {
               $(this).addClass("past").removeClass("present future");
@@ -32,10 +40,6 @@ $(document).ready(function () {
         })
     };
 
-   for (var i = 8; i <= 18; ++i) {
-    $("#hour-" + i + ".description").val(localStorage.getItem("#hour-" + i));
-   }
-
-    timeTense();
+    colorTracker();
 });
 
